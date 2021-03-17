44888e8f8248a28c531174ec40b9b344395d7e74
mirzahamza1 <56633470+mirzahamza1@users.noreply.github.com>
2021-03-17 22:22:11 +0500

    Add files via upload

---
+++ rotate-4174.py
@@ -0,0 +1,16 @@
+from matplotlib.pyplot import imread
+import matplotlib.pyplot as plt
+from scipy.ndimage.filters import convolve
+import numpy as np
+
+k3 = np.array([[1,1,1], [1,1,1], [1,1,1]])
+img = imread("car.jpg")
+channels = []
+for channel in range(3):
+    res = convolve(img[:,:,channel], k3)
+    channels.append(res)
+
+img = np.dstack((channels[0], channels[1], channels[2]))
+plt.imshow(img)
+plt.show()
+
